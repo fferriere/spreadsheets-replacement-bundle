d9c2f13f0eb1db365478ff062bf6c5827f4f8477
FERRIERE Florian <ferriere.florian@gmail.com>
2014-09-19 10:23:33 +0200

    Creating empty bundle with console

---
+++ FferriereSpreadsheetsReplacementBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Fferriere\Bundle\SpreadsheetsReplacementBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FferriereSpreadsheetsReplacementBundle extends Bundle
+{
+}
